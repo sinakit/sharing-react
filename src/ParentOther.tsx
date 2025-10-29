292a01115b30136ab924526f6e2c086f9c49a535
sinakit <hin.sinak@gmail.com>
2025-10-29 15:38:55 +0700

    update

--- src/ParentOther.tsx
+++ src/ParentOther.tsx
@@ -2,24 +2,26 @@ import { useEffect, useState } from "react";
 import { useParentState } from "./states/useParent";
 
 const ParentOther = () => {
-    const {globalCounter, setGlobalCounter} = useParentState();
-const [fullName, setFullName] = useState<string>("ABC");
-const increaseCounter = () => {
+  const { globalCounter, setGlobalCounter } = useParentState();
+  const [fullName, setFullName] = useState<string>("ABC");
+  const increaseCounter = () => {
     setGlobalCounter(globalCounter + 1);
-}
-
+  };
 
   return (
     <div>
-        <h2>Parent Other Component</h2>
-        <h3>Parent Other Counter: {globalCounter}</h3>
-        <button onClick={()=>{setFullName("Cheav Ratana")}}>Change to Cheav Ratana</button>
-        <button onClick={increaseCounter}>Increase Counter Parent Other</button>
-        {/* <Child fullName='Cheav A'  />
-        <Child fullName='Cheav B' /> */}
-        </div>
-    )
-}
-
+      <h2>Parent Other Component</h2>
+      <h3>Parent Other Counter: {globalCounter}</h3>
+      <button
+        onClick={() => {
+          setFullName("Cheav Ratana");
+        }}
+      >
+        Change to Cheav Ratana
+      </button>
+      <button onClick={increaseCounter}>Increase Counter Parent Other</button>
+    </div>
+  );
+};
 
 export default ParentOther;
