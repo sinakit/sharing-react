c17d50faf66892472db4728c51ab8c1c5d23bc95
sinakit <hin.sinak@gmail.com>
2025-11-10 22:25:14 +0700

    remove unused code

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { RecoilRoot } from 'recoil'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RecoilRoot>
       <App />
-    </RecoilRoot>
   </StrictMode>,
 )
